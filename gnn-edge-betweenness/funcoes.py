62ed9b6b1158cbcd0539067a5f79ec7548caa6f5
ana <analuizaas77@gmail.com>
2024-11-28 16:06:03 -0300

    Desenvolvimento do projeto de redes

--- gnn-edge-betweenness/funcoes.py
+++ gnn-edge-betweenness/funcoes.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import time
+import random
 
 
 def read_networks (networks):
@@ -14,7 +15,7 @@ def read_networks (networks):
     
     return n 
 
-def betweenness_redes (networks):
+def edge_betweenness_networks (networks):
     times  = []
     betweenness = []
     for net in networks:
@@ -24,3 +25,28 @@ def betweenness_redes (networks):
         elapsed_time = time.time() - start_time
         times.append(elapsed_time)
     return betweenness, times
+
+
+def edge_select (network, tx=0.4):
+    if not (0 <= tx <= 1):
+        raise ValueError('A porcentagem deve estar entre 0 e 1')
+    
+    edges = list(network.edges)
+    print(len(edges))
+    n = max(1, int(tx * len(edges)))
+    print(n)
+    selected_edges = random.sample(edges, n)
+    
+    return selected_edges
+
+def edge_selected_betweenness (network, selected_edges):
+    bet = {edge: 0.0 for edge in selected_edges}
+
+    return bet
+
+def propose_model(network):
+    start_time = time.time()
+    selected_edges = edge_select(network)
+    bet = edge_selected_betweenness(network, selected_edges)
+    t = time.time() - start_time 
+    return bet, t
